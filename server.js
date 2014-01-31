7f29facb0c96ea6c4c316d7614bc8fc5e8f2092f
Brian Genisio <briangenisio@gmail.com>
2014-01-31 02:54:59 +0000

    Adding a little REST service for getting/setting pins

--- server.js
+++ server.js
@@ -2,13 +2,39 @@ var leds = require('./leds');
 var http = require('http');
 var connect = require('connect');
 
-var app = connect()
-  .use(connect.static('public'));
+var routes = {
+	'GET:/api/leds': getLEDs,
+	'POST:/api/leds': updateLED
+}
 
+function getLEDs(req, resp) {
+	resp.end(JSON.stringify(leds.leds));
+}
 
-function startServer() {
-	console.log('listening on 3000...');
-	http.createServer(app).listen(3000);
+function updateLED(req, resp) {
+	var led = req.body;
+	leds.set(led.pin, led.value);
+	resp.end(JSON.stringify(leds.leds));
 }
 
 leds.init(startServer);
+
+
+
+
+var app = connect()
+  .use(connect.static('public'))
+  .use(connect.urlencoded())
+  .use(connect.json())
+  .use(dataService);
+
+function dataService(req, resp, next) {
+	var route = req.method + ":" + req.url;
+	if(!routes[route]) return next();
+
+	routes[route](req, resp);
+}
+
+function startServer() {
+	http.createServer(app).listen(3000);
+}
